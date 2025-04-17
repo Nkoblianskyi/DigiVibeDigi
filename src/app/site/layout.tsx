97d56ba6db63bf90ffb2491f3ea93203c5792b3e
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-04-17 21:21:11 +0200

    13

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -1,7 +1,10 @@
+
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="es">
-            <body style={{ margin: 0, padding: 0 }}>{children}</body>
+            <body style={{ margin: 0, padding: 0 }}>
+                {children}
+            </body>
         </html>
     );
 }
