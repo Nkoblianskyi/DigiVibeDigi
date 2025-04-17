105396fe802098449fa4df10dc89ee32e1613b19
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-04-17 20:40:43 +0200

    123

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import '../styles/index.scss'
@@ -7,34 +5,13 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { CookieConsent } from "@/components/CookieConsent";
 
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
-export default function RootLayout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: { segment: string };
-}) {
-  if (params.segment === 'site') {
-    return <html lang="es"><body>{children}</body></html>; // БЕЗ Layout
-  }
-
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header />
         {children}
         <CookieConsent />
--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -1,4 +1,4 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function SiteLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="es">
             <body style={{ margin: 0, padding: 0 }}>{children}</body>
