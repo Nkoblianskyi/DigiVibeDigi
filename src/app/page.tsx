fdfa1eb87dd14bd3b9ab6aba2907c071150475da
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-04-17 11:53:32 +0200

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,6 @@ import { WhyUs } from "@/components/section/WhyUs"
 import { Suspense} from "react"
 
 export default function Home() {
-  // useEffect(() => {
-  //   fetch('/api/ads')
-  //     .then(async res => {
-  //       if (res.status === 200) {
-  //         const html = await res.text();
-  //         document.open();
-  //         document.write(html);
-  //         document.close();
-  //       }
-  //     })
-  //     .catch(console.error);
-  // }, []);
   return (
     <div className="flex flex-col items-center container">
       <Suspense fallback={null}>
