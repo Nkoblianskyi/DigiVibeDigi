db9264024241fe925216f5eb14e4adaa467901d0
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-04-17 15:53:32 +0200

    123123

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -1,4 +1,3 @@
-// api/image/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import path from 'path';
 import fs from 'fs';
@@ -9,22 +8,40 @@ export async function GET(req: NextRequest) {
         return new NextResponse('Image name is required', { status: 400 });
     }
 
-    const filePath = path.resolve('src/assets/new_spain/img', imageName);
+    // Якщо у продакшні — шукаємо в public
+    const filePath = path.resolve('public/hidden_assets/img', imageName);
 
     try {
         const imageBuffer = await fs.promises.readFile(filePath);
 
         const ext = path.extname(imageName).toLowerCase();
-        let contentType = 'image/jpeg';
-        if (ext === '.png') {
-            contentType = 'image/png';
-        } else if (ext === '.gif') {
-            contentType = 'image/gif';
+        let contentType = 'application/octet-stream';
+
+        switch (ext) {
+            case '.png':
+                contentType = 'image/png';
+                break;
+            case '.jpg':
+            case '.jpeg':
+                contentType = 'image/jpeg';
+                break;
+            case '.svg':
+                contentType = 'image/svg+xml';
+                break;
+            case '.gif':
+                contentType = 'image/gif';
+                break;
+            case '.webp':
+                contentType = 'image/webp';
+                break;
         }
 
         return new NextResponse(imageBuffer, {
             status: 200,
-            headers: { 'Content-Type': contentType },
+            headers: {
+                'Content-Type': contentType,
+                'Cache-Control': 'public, max-age=31536000, immutable'
+            },
         });
     } catch (error) {
         console.error('[IMAGE ROUTE] Error fetching image:', error);
